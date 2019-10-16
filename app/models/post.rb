9d7d153966933f664a9e66387c8b5982df800722
Daniel Park <parkdk96@gmail.com>
2019-10-16 10:14:41 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+# Clickbait attribute being validated
+class ClickbaitValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    # Regex, matches a string followed by (b|c)
+    # The \d matches a single character that is a digit (Top 10)
+    # =~ operator matches regex against a string
+    # the i at the end makes the expression case insensitive
+    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/i
+      record.errors[attribute] << ("Not a clickbait title")
+    end
+  end
+end
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true, clickbait: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  
 end
